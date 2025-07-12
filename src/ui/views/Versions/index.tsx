0c5b4c074a17d91f9dc5c2921e3e826392aab405
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-07-12 16:52:08 +0800

    左侧导航栏整体配置

--- src/ui/views/Versions/index.tsx
+++ src/ui/views/Versions/index.tsx
@@ -1,6 +1,10 @@
 import styles from './index.module.scss'
 
 const Versions = () => {
-  return <div className={styles.versions}>123</div>
+  return (
+    <div className={styles.versions}>
+      <span>多云RA v1.0.0</span>
+    </div>
+  )
 }
 export default Versions
