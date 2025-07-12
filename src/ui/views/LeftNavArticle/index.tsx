2419e2e0025252e52210a00b47fa1948827c38b1
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-07-12 16:49:21 +0800

    nav暂时样式

--- src/ui/views/LeftNavArticle/index.tsx
+++ src/ui/views/LeftNavArticle/index.tsx
@@ -1,6 +1,14 @@
 import styles from './index.module.scss'
 
 const LeftNavArticle = () => {
-  return <div className={styles.leftNavArticle}>LeftNavArticle</div>
+  return (
+    <div className={styles.leftNavArticle}>
+      <div className={styles.leftNavArticleItem}>配置</div>
+      <div className={styles.leftNavArticleItem}>BOSS直聘</div>
+      <div className={styles.leftNavArticleItem}>牛客网</div>
+      <div className={styles.leftNavArticleItem}>关于</div>
+      <div className={styles.leftNavArticleItem}>日志</div>
+    </div>
+  )
 }
 export default LeftNavArticle
