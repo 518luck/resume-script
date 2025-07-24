cb1cac19497b0f2115a17ebdc61179cb597946dd
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-07-25 01:11:41 +0800

    修改日志bug

--- src/ui/utils/parseLogWithColors.ts
+++ src/ui/utils/parseLogWithColors.ts
@@ -3,7 +3,7 @@ export const parseLogWithColors = (logText: string) => {
 
   return logText.replace(
     /(\[INFO\]|\[WARNING\]|\[ERROR\])([^\n]*)/g,
-    (tag, content) => {
+    (_, tag, content) => {
       const level = tag.slice(1, -1)
       const className = `log-${level.toLowerCase()}`
       return `<span class="${className}">${tag}${content}</span>`
